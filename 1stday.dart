cf505e1076eb55b676207a33329eef62b43930e7
Babar Hans <babarhans34@gmail.com>
2023-10-25 22:18:17 +0500

    class code of flutter

---
+++ 1stday.dart
@@ -0,0 +1,77 @@
+
+import 'dart:ffi';
+
+void main(){
+  // print("hello world");
+  // int number1= 1;
+  // int number2=2;
+  // int sum= number1+number2;
+  // print(sum);
+  // const String name = "babar";
+  // print(name);
+  // const bool = false;
+  // double height = 5.1;
+  // print(height);
+  
+  // List<String> friends=[
+  //   "Babar",
+  //   "Sahil",
+  //   "Asad"
+  // ]; 
+
+  // List<int> marks = [0,1,2,3,4,5,6];
+  // marks[0]= 10;
+  // print(marks[0]+marks[1]);
+  // marks = [10,11,12,13,14,15];
+  // print(marks);
+
+  // two dimensional lists
+  
+  // List<String> Section2 = ["ok","Good","Dart","Hello"];
+
+  // List <List<String>> Section = [
+  //     Section2,
+  //     ["Babar","Asad","Noman","Sahil"]
+   
+  // ];
+  // // RC R=Row and C = Col
+  // print(Section[0][1]);
+  // print(Section[1][0]);
+
+  // Map<String,int> marks = {
+  //   "Babar": 100, "Asad": 200, "Sahil":50
+  // };
+  // print(marks["Babar"]);
+
+//  List<Map<String,int>> mark = [
+   
+//    {"ishaq":100, "Hassan": 50},
+//    {"ishaq":100, "Hassan": 50},
+//  ]; 
+//  mark[0]["Hassan"] = 200;
+//  print(mark[0]["Hassan"]);
+
+// control flow
+// int num1 = 1;
+// if(num1 == 10){
+//   print("Babar");
+// }
+// else{
+//   print("wrong");
+// }
+
+int engineSize = 600;
+String color = "white";
+
+switch(engineSize){
+  case 500:
+  print("500 lelo");
+  break;
+  case 600:
+  print("600 lelo");
+  break;
+  default:
+  print("koi bhi nai lena");
+  
+}
+ }
